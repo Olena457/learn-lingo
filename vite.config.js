5d4a005b84678ee36193cdc36d5efbe356d55bf5
Olena457 <olenademianenko42@gmail.com>
2024-12-05 00:46:46 +0200

    deleted config

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { terser } from '@rollup/plugin-terser';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks: id => {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-          return 'main';
-        },
-      },
-      plugins: [
-        terser({
-          compress: {
-            drop_console: true, // Видаляє консольні логування
-          },
-          format: {
-            comments: false, // Видаляє коментарі
-          },
-        }),
-      ],
-    },
+    minify: 'esbuild',
+    sourcemap: true,
   },
 });
