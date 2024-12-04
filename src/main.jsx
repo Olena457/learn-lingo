59c1c8bed71e8756521c37817ea2d91cb37891f7
Olena457 <olenademianenko42@gmail.com>
2024-12-04 02:19:18 +0200

    optimized icon

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,9 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <Provider store={store}>
         <App />
       </Provider>
