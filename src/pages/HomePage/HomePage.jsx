09816e772aecad9c527bc0e9bae49f1b1fd71ff4
Olena457 <olenademianenko42@gmail.com>
2024-12-04 03:35:54 +0200

    updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,22 +1,23 @@
-import css from './HomePage.module.css';
-import HeroSection from '../../components/HeroSection/HeroSection.jsx';
-import heroLarge from '/hero-large.png';
-import FeaturesDisplay from './../../components/FeaturesDisplay/FeaturesDisplay.jsx';
+import css from "./HomePage.module.css";
+import HeroSection from "../../components/HeroSection/HeroSection.jsx";
+import heroLarge from "/hero-large.png";
+import FeaturesDisplay from "./../../components/FeaturesDisplay/FeaturesDisplay.jsx";
 
 const HomePage = () => {
-  return (
-    <main className="css.main">
-      <div className={css.heroContainer}>
-        <HeroSection />
-        <img
-          src={heroLarge}
-          alt="User photo with laptop"
-          className={css.photo}
-        />
-      </div>
-      <FeaturesDisplay />
-    </main>
-  );
+    return (
+        <main className="css.main">
+            <div className={css.heroContainer}>
+                <HeroSection />
+                <img
+                    src={heroLarge}
+                    alt="User photo with laptop"
+                    className={css.photo}
+                />
+            </div>
+            <FeaturesDisplay />
+        </main>
+    );
 };
 
 export default HomePage;
+// future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
--- src/redux/favorites/sliceFavorites.js
+++ src/redux/favorites/sliceFavorites.js
@@ -1,50 +1,53 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { fetchFavorites, toggleFavorite } from './operationsFavorites.js';
-import { logoutUser } from '../auth/operationsAuth.js';
+import { createSlice } from "@reduxjs/toolkit";
+import {
+    fetchFavorites,
+    toggleFavorite,
+} from "../favorites/operationsFavorites.js";
+import { logoutUser } from "../auth/operationsAuth.js";
 
-const handlePending = state => {
-  state.loading = true;
-  state.error = null;
+const handlePending = (state) => {
+    state.loading = true;
+    state.error = null;
 };
 
 const handleRejected = (state, action) => {
-  state.loading = false;
-  state.error = action.payload;
+    state.loading = false;
+    state.error = action.payload;
 };
 
 const favoritesInitialState = {
-  items: [],
-  loading: false,
-  error: null,
+    items: [],
+    loading: false,
+    error: null,
 };
 
 const favoritesSlice = createSlice({
-  name: 'favorites',
-  initialState: favoritesInitialState,
-  reducers: {
-    setFavorites: (state, action) => {
-      state.items = action.payload;
+    name: "favorites",
+    initialState: favoritesInitialState,
+    reducers: {
+        setFavorites: (state, action) => {
+            state.items = action.payload;
+        },
     },
-  },
-  extraReducers: builder => {
-    builder
-      //_____________fetch favorites teacher
-      .addCase(fetchFavorites.pending, handlePending)
-      .addCase(fetchFavorites.fulfilled, (state, action) => {
-        state.items = action.payload;
-        state.loading = false;
-      })
-      .addCase(fetchFavorites.rejected, handleRejected)
-      //____________toggle favorite teacher
-      .addCase(toggleFavorite.pending, handlePending)
-      .addCase(toggleFavorite.fulfilled, (state, action) => {
-        state.items = action.payload;
-        state.loading = false;
-      })
-      .addCase(toggleFavorite.rejected, handleRejected)
-      //_______________logout
-      .addCase(logoutUser.fulfilled, () => favoritesInitialState);
-  },
+    extraReducers: (builder) => {
+        builder
+            //_____________fetch favorites teacher
+            .addCase(fetchFavorites.pending, handlePending)
+            .addCase(fetchFavorites.fulfilled, (state, action) => {
+                state.items = action.payload;
+                state.loading = false;
+            })
+            .addCase(fetchFavorites.rejected, handleRejected)
+            //____________toggle favorite teacher
+            .addCase(toggleFavorite.pending, handlePending)
+            .addCase(toggleFavorite.fulfilled, (state, action) => {
+                state.items = action.payload;
+                state.loading = false;
+            })
+            .addCase(toggleFavorite.rejected, handleRejected)
+            //_______________logout
+            .addCase(logoutUser.fulfilled, () => favoritesInitialState);
+    },
 });
 
 export const { setFavorites } = favoritesSlice.actions;
