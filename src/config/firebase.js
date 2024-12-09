8081935762d2818ea3f8d5e27ff3d4078ec073c0
Olena457 <olenademianenko42@gmail.com>
2024-12-09 04:52:37 +0200

    fixed

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -22,4 +22,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
-export const auth = getAuth(app);
+export const auth = getAuth();
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'modern-normalize/modern-normalize.css';
+// import 'modern-normalize/modern-normalize.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from './components/ThemeProvider/ThemeProvider.jsx';
+// import { ThemeProvider } from './components/ThemeProvider/ThemeProvider.jsx';
 import store from './redux/store.js';
 import './index.css';
 import App from './App.jsx';
@@ -20,9 +20,9 @@ createRoot(document.getElementById('root')).render(
       }}
     >
       <Provider store={store}>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
+        {/* <ThemeProvider> */}
+        <App />
+        {/* </ThemeProvider> */}
       </Provider>
     </BrowserRouter>
   </StrictMode>
